c58fe64ce76c8a3ddf15ef607c26270460ab8f35
Jonny Hughes <me@jonnyhugh.es>
2019-03-11 20:01:00 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #! /bin/sh -l
 api_version=3
 api_root='https://api.github.com'
-commit_count=`curl -H "Authorization: token ${GITHUB_TOKEN}" "${api_root}/repos/j0nnyhughes/alpha/pulls/1/commits" | grep -o '"commit": {' | wc -l`
+commit_count=`curl -H "Authorization: token $GITHUB_TOKEN" "${api_root}/repos/j0nnyhughes/alpha/pulls/1/commits" | grep -o '"commit": {' | wc -l`
 echo "commits: ${commit_count}"
 
 if [ $commit_count -gt 4 ]
