71e6232a4acfefc7561bc9e8b8a2ee8be5a22315
Jonny Hughes <me@jonnyhugh.es>
2019-03-11 20:07:30 +0000

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,15 @@
 #! /bin/sh -l
 api_version=3
 api_root='https://api.github.com'
-commit_count=`curl -H "Authorization: token $GITHUB_TOKEN" "${api_root}/repos/j0nnyhughes/alpha/pulls/1/commits" | grep -o '"commit": {' | wc -l`
+
+if [[ -z "$GITHUB_TOKEN" ]]; then
+	echo "Set the GITHUB_TOKEN env variable."
+	exit 1
+fi
+
+AUTH_HEADER="Authorization: token ${GITHUB_TOKEN}"
+
+commit_count=$(curl -s -H "${AUTH_HEADER}" "${api_root}/repos/j0nnyhughes/alpha/pulls/1/commits" | grep -o '"commit": {' | wc -l)
 echo "commits: ${commit_count}"
 
 if [ $commit_count -gt 4 ]
