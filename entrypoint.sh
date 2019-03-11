e55bbb2609813f63c0812292831d0cdfe847a6de
Jonny Hughes <me@jonnyhugh.es>
2019-03-11 19:40:40 +0000

    commit count

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,12 @@
-#!/bin/sh -l
+#! /bin/sh -l
+api_version=3
+api_root='https://api.github.com'
+commit_count=`curl -s -H "Authorization: token ${GITHUB_TOKEN}" "${api_root}/repos/j0nnyhughes/alpha/pulls/1/commits" | grep -o '"commit": {' | wc -l`
+echo "commits: ${commit_count}"
 
-sh -c "echo $*"
+if [ $commit_count -gt 4 ]
+then 
+    echo "INTERACTIVE REBASE YOU MUPPET"
+else
+    echo "LOOKS GOOD"
+fi
